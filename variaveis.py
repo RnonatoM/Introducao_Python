7ceb06299c5e88cfc7ec0a166d507cc92efbb3a0
Raimundo Nonato Maia <112270577+RnonatoM@users.noreply.github.com>
2023-08-09 16:44:26 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+idade = 26 # Isso é uma variavel, chamada "Idade".
+print(idade)
+
+nome = "Raimundo Nonato" 
+print(nome)
+
+"""
+Tipos de Variaveis
+int: números inteiros, ou seja, números sem parte decimal.
+float: números reais, ou seja, números com partes decimal.
+str: cadeias de caracteres, ou seja, dados textuais.
+bool: valores logicos (booleanos): True ou False.
+"""

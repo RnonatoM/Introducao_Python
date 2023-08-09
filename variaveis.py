2b2d7e24f0320877e41fe13a8c511cffa4781647
Raimundo Nonato Maia <112270577+RnonatoM@users.noreply.github.com>
2023-08-09 17:01:29 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+idade = 26 # Isso é uma variavel, chamada "Idade".
+print(idade)
+
+nome = "Raimundo Nonato" 
+print(nome)
+
+"""
+Tipos de Variaveis
+int: números inteiros, ou seja, números sem parte decimal.
+float: números reais, ou seja, números com partes decimal.
+str: cadeias de caracteres, ou seja, dados textuais.
+bool: valores logicos (booleanos): True ou False.
+"""
+
+idade = 26
+altura = 1.77
+nome = "Raimundo Nonato"
+estudando = True 
+
+print(type(idade)) #Aqui estou imprimindo o tipo da variavel "idade".
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+print("----------------------------------------")
+
+#Obtendo dados do usuario e salvando em variaveis
+linguagem = input("Qual é a linguagem de programação que voce está estudando? ")
+print("A linguagem que voce esta estudando é", linguagem)

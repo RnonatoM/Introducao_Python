fbb0fa2d68ff6548d4360b86f86b768b8249d443
Raimundo Nonato Maia <112270577+RnonatoM@users.noreply.github.com>
2023-08-10 15:22:00 -0300

    Add files via upload

---
+++ estruturascondicionais.py
@@ -0,0 +1,33 @@
+# ESTRUTURAS CONDICIONAIS 
+
+"""idade = 20
+
+if idade >= 18:
+    print('Voce é maior de idade.')
+else:
+    print("Voce é menor de idade.")
+
+"""
+"""
+Imagine que deseje imprimir "Aprovado(o)", caso o estudante tenha uma média 
+superior ou igual a 7, e "Reprovado", caso a média sejá inferior a 7.
+"""
+"""
+
+media = float(input ("Informe a média do estudante: "))
+
+if media >= 7:
+    print("voce foi aprovada(o)!")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("voce foi reprovado(o).")"""
+
+
+media = 10
+presenca = 100
+
+if media >= 7 and presenca >= 70:
+    print("Aprovado!")
+else:
+    print("Reprovado")

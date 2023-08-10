492a56685dbb40e3d23012250b57294b64fe9736
Raimundo Nonato Maia <112270577+RnonatoM@users.noreply.github.com>
2023-08-10 14:40:14 -0300

    Add files via upload

---
+++ conversaodtipos.py
@@ -0,0 +1,17 @@
+# Conversão de Tipos 
+
+idade   = '26'
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+# Além da função int()
+# Ah, também a função str()
+#  float() 
+#  bool()
+
+altura = float( input('informe a sua altura: '))
+print(altura, type(altura))
